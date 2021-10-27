ad8f34885591724c978bd7ba259cb6785216b063
Sergio Torres <sergio.lo.torres@gmail.com>
2021-10-26 23:34:05 -0500

    pseudologic for pathway in progress

---
+++ client/src/utils/pathways.js
@@ -0,0 +1,16 @@
+
+
+// here we run the logic to chain each of the books
+
+// each chapter is assigned an id when is created, the id is correspondent to the order of the story inside the book
+// chapter 1 will be 1.a  which will be yield options to move towards 2a, 2.b , 2.c, 2,d ( think of it as levels in a [pyramid])
+// chapter 2.b will connect to lvls 3.a 3b 3.c and so on.
+// a visual layer will be necessary in order to make this possible. more to that later
+// this being said. each pathway is a object? with a linear track of chapter 1.a into 2.c into 3.d etc stored in database
+
+export function Pathways(){
+
+
+
+
+}
